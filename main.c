2899862929a02f61cd00309c7fe02fd3503b5530
Sahiiil1406 <supersen@gmail.com>
2025-06-17 21:59:13 +0530

    cursor added

--- main.c
+++ main.c
@@ -68,7 +68,11 @@ typedef struct {
   Pager * pager;
 } Table;
 
-
+typedef struct {
+  Table* table;
+  uint32_t row_num;
+  bool end_of_table;  // Indicates a position one past the last element
+} Cursor;
 
 typedef struct {
   char* buffer;
@@ -81,6 +85,21 @@ typedef struct {
   Row row_to_insert;
 } Statement;
 
+Cursor* table_start(Table* table) {
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->row_num = 0;
+  cursor->end_of_table = (table->num_rows == 0);
+  return cursor;
+}
+
+Cursor* table_end(Table* table) {
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->row_num = table->num_rows;
+  cursor->end_of_table = true;
+  return cursor;
+}
 Pager *pager_open(const char* filename){
   int fd=open(filename,O_RDWR | O_CREAT,0644);
   if(fd==-1){
@@ -170,20 +189,20 @@ void* get_page(Pager* pager, uint32_t page_num) {
 
   return pager->pages[page_num];
 }
-void* row_slot(Table* table, uint32_t row_num) {
+void* cursor_value(Cursor* cursor) {
+  uint32_t row_num = cursor->row_num;
   uint32_t page_num = row_num / ROWS_PER_PAGE;
-  //void* page = table->pages[page_num];
-  // if (page == NULL) {
-  //   page = table->pages[page_num] = malloc(PAGE_SIZE);
-  //   if (page == NULL) {
-  //     printf("Memory allocation error\n");
-  //     exit(EXIT_FAILURE);
-  //   }
-  // }
-  void* page = get_page(table->pager, page_num);
+  void* page = get_page(cursor->table->pager, page_num);
   uint32_t row_offset = row_num % ROWS_PER_PAGE;
   uint32_t byte_offset = row_offset * ROW_SIZE;
   return page + byte_offset;
+}
+
+void cursor_advance(Cursor* cursor) {
+  cursor->row_num += 1;
+  if (cursor->row_num >= cursor->table->num_rows) {
+    cursor->end_of_table = true;
+  }
 }
 
 InputBuffer* new_input_buffer() {
@@ -263,23 +282,26 @@ void print_row(Row* row) {
   printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
-
 ExecuteResult execute_insert(Statement *statement, Table* table){
   if(table->num_rows >= TABLE_MAX_ROWS){
     return EXECUTE_TABLE_FULL;
   }
   Row *row_to_insert = &(statement->row_to_insert);
-  serialize_row(row_to_insert, row_slot(table, table->num_rows));
+  Cursor* cursor = table_end(table);
+  serialize_row(row_to_insert, cursor_value(cursor));
   table->num_rows += 1;
   return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement *statement, Table *table){
+  Cursor* cursor = table_start(table);
   Row row;
-  for (uint32_t i = 0; i < table->num_rows; i++) {
-     deserialize_row(row_slot(table, i), &row);
-     print_row(&row);
+  while (!(cursor->end_of_table)) {
+    deserialize_row(cursor_value(cursor), &row);
+    print_row(&row);
+    cursor_advance(cursor);
   }
+  free(cursor);
   return EXECUTE_SUCCESS;
 }
 
